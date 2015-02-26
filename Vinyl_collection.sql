cf69cacdce4a7a195f95da569f43162e2fc5e31c
Yael <slinkyloverhere@gmail.com>
2015-02-26 10:06:53 +0100

    termino de hacer los inserts

--- Vinyl_collection.sql
+++ Vinyl_collection.sql
@@ -69,6 +69,21 @@ INSERT `Sitio` VALUES ('01/12', '01','12');
 INSERT `Sitio` VALUES ('03/24', '03','24');
 INSERT `Sitio` VALUES ('02/05', '2','5');
 
+
+#Conservacion
+
+CREATE TABLE `Conservacion` (
+  `idConservacion` varchar(15) NOT NULL ,
+  `Conservacion` varchar(10) NOT NULL ,
+  PRIMARY KEY  (`idConservacion`)
+) ENGINE=InnoDB;
+
+INSERT `Conservacion` VALUES ('M', 'Mint');
+INSERT `Conservacion` VALUES ('VG', 'Very Good');
+INSERT `Conservacion` VALUES ('G', 'Good');
+
+
+
 #-----------------------------------
 
 # Disco
@@ -93,7 +108,7 @@ INSERT `Disco` VALUES ('Salad Days_MD','Salad Days','2014','Mac Demarco');
 CREATE TABLE `Edicion` (
   `idEdicion` varchar(30) NOT NULL ,
   `idDisco` varchar(40) NOT NULL ,
-  `Catalogo` varchar(40) NOT NULL ,
+  `NumCatalogo` varchar(40) NOT NULL ,
   `Año` varchar(4) NOT NULL,
   `Lugar` varchar(10) NOT NULL, 
   `Formato` varchar(6) NOT NULL,
@@ -105,6 +120,10 @@ CREATE TABLE `Edicion` (
   CONSTRAINT `FK_edicion_formato` FOREIGN KEY (`Formato`) REFERENCES `Formato` (`idFormato`)
 ) ENGINE=InnoDB;
 
+INSERT `Edicion` VALUES ('USA_2003_12','Elephant_WS','0123-123','2003', 'USA','12','Modular');
+INSERT `Edicion` VALUES ('UK_2012_10','Elephant_TI','2364-105','2012','D','10','V2');
+INSERT `Edicion` VALUES ('D_2014_12','Salad Days_MD','3046-013','2014', 'D','12','Cap Tracks');
+
 
 # Ejemplar
 
@@ -112,20 +131,20 @@ CREATE TABLE `Ejemplar` (
   `idEjemplar` varchar(30) NOT NULL ,
   `idEdicion` varchar(30) NOT NULL ,
   `idDisco` varchar(40) NOT NULL ,
-  `Estado de conservacion` varchar(4) NOT NULL ,
-  `Precio Compra` varchar(4) NOT NULL,
+  `Conservacion` varchar(4) NOT NULL ,
+  `Precio Compra` varchar(10) NOT NULL,
   `Sitio` varchar(10) NOT NULL, 
   PRIMARY KEY  (`idEjemplar`,`idEdicion`,`idDisco`),
   CONSTRAINT `FK_ejemplar_disco` FOREIGN KEY (`idDisco`) REFERENCES `Disco` (`idDisco`),
   CONSTRAINT `FK_ejemplar_edicion` FOREIGN KEY (`idEdicion`) REFERENCES `Edicion` (`idEdicion`),
+  CONSTRAINT `FK_ejemplar_conservacion` FOREIGN KEY (`Conservacion`) REFERENCES `Conservacion` (`idConservacion`),
   CONSTRAINT `FK_ejemplar_sitio` FOREIGN KEY (`Sitio`) REFERENCES `Sitio` (`idSitio`)
 ) ENGINE=InnoDB;
 
 
-
-
-
-
+INSERT `Ejemplar` VALUES ('#1','USA_2003_12','Elephant_WS','VG','20,00€','01/12');
+INSERT `Ejemplar` VALUES ('#2','UK_2012_10','Elephant_TI','M','10,00€','03/24');
+INSERT `Ejemplar` VALUES ('#1','D_2014_12','Salad Days_MD','G','30,00€','02/05');
 
 
 
